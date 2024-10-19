068daba72337674e45feacaff4c4fb0ea609bf3f
igormintz <igormintz@gmail.com>
2024-10-19 21:57:05 +0300

    added example video and debug results

--- highlights/app.py
+++ highlights/app.py
@@ -1,18 +1,20 @@
 import argparse
 import logging
-from video_processing import extract_keypoints, create_highlight_lists, add_intro_and_outro, create_video_segments
+from video_processing import extract_keypoints, create_highlight_lists, add_intro_and_outro, create_video_segments, overlay_keypoints_on_frames
 from keypoints_processing import process_keypoints
 from pathlib import Path
 
-def main(input_path, output_path):
+def main(input_path: Path, output_path: Path, save_debug=False):
     # TODO: add validations (file exists, extensions)
     logging.info("Starting video processing")
     
     logging.info("Extracting keypoints")
-    df, frames = extract_keypoints(Path(input_path), Path(output_path))
+    df, frames, fps = extract_keypoints(input_path, output_path, save_debug)
     
+    if save_debug:
+        overlay_keypoints_on_frames(df, frames, fps, output_path)
     logging.info("Processing keypoints")
-    df = process_keypoints(df, Path(output_path))
+    df = process_keypoints(df, output_path, fps, save_debug)
     
     logging.info("Extracting frames from processed data")
     frames = df['frame'].to_list()
@@ -24,7 +26,7 @@ def main(input_path, output_path):
     highlight_frame_list = add_intro_and_outro(highlight_frame_list)
     
     logging.info("Creating video segments")
-    create_video_segments(highlight_frame_list, frames, Path(output_path), Path(input_path))
+    create_video_segments(highlight_frame_list, frames, output_path, input_path)
     
     logging.info("Video processing completed")
 
@@ -32,8 +34,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process video keypoints")
     parser.add_argument("--input_path", required=True, help="Path to the input video file")
     parser.add_argument("--output_path", required=True, help="Path to save intermediate CSV files")
+    parser.add_argument("--save_debug", action="store_true", default=False, help="Save debug information")
+    
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     
-    main(args.input_path, args.output_path)
+    main(Path(args.input_path), Path(args.output_path), args.save_debug)
--- highlights/keypoints_processing.py
+++ highlights/keypoints_processing.py
@@ -1,7 +1,8 @@
 import polars as pl
 from pathlib import Path
-DIFF_QUANTILE = 0.75
+DIFF_QUANTILE = 0.75 # TODO: optimize
 FRAME_DIFF = 40
+MIN_SECONDS_PER_PERSON = 2
 
 def add_diffs(df: pl.DataFrame) -> pl.DataFrame:
     return df.with_columns(
@@ -17,15 +18,25 @@ def replace_zeros_with_nans(df: pl.DataFrame) -> pl.DataFrame:
 def filter_by_percentile(df: pl.DataFrame) -> pl.DataFrame:
     return df.filter(pl.col("max_diff") > pl.col("max_diff").quantile(DIFF_QUANTILE))
 
-def save_intermediate_csv(df: pl.DataFrame, output_path: Path) -> pl.DataFrame:
-    df.write_parquet(output_path/ "processed_keypoints_data.parquet")
-    return df
+def filter_by_datapoints_per_person(df: pl.DataFrame, fps: int) -> pl.DataFrame:
+    """remove persons with appearence shorter than MIN_SECONDS_PER_PERSON"""
+    return df.filter(
+        pl.col('person').is_in(
+            df['person'].value_counts().filter(pl.col('count')> fps*MIN_SECONDS_PER_PERSON)['person']
+        )
+    )
 
 def save_chart(df: pl.DataFrame, output_path: Path) -> pl.DataFrame:
     df.plot.line(x="frame", y="y_Right Knee", color="person").save(output_path/ "chart.png")
     return df
 
-def process_keypoints(df: pl.DataFrame, output_path: Path) -> pl.DataFrame:
+def save_df(df, output_path: Path, name: str, save=False,) -> None:
+    "save a parquet file with all of the extracted skeleton keypoints"
+    if save:
+        df.write_parquet(output_path / f"{name}.parquet")
+    return df
+
+def process_keypoints(df: pl.DataFrame, output_path: Path, fps:int, save_debug=False) -> pl.DataFrame:
     df = (
         df
         .pivot(on="keypoint", values=["x", "y"], index=["person", "frame"])
@@ -35,6 +46,8 @@ def process_keypoints(df: pl.DataFrame, output_path: Path) -> pl.DataFrame:
         .fill_null(strategy="forward")
         .with_columns(pl.col("person").cast(pl.Utf8))
         .pipe(add_diffs)
+        .pipe(save_df, output_path, "all_keypoints", save_debug)
+        .pipe(filter_by_datapoints_per_person, fps)
         .pipe(save_chart, output_path)
     )
     df = (
@@ -45,8 +58,8 @@ def process_keypoints(df: pl.DataFrame, output_path: Path) -> pl.DataFrame:
         .unique('frame')
         .sort("frame")
         .with_columns(frame_diff=pl.col("frame").diff())
-        .pipe(save_intermediate_csv, output_path)
         .filter(pl.col("frame_diff")<=FRAME_DIFF)
+        .pipe(save_df, output_path, "processed_keypoints_data", save_debug)
     )
     return df
 
